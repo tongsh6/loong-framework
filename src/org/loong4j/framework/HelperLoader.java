05eb5d6529884a4a3635b0d7c58445de89b029df
tongsh6 <tongsh6@qq.com>
2016-05-25 21:42:58 +0800

    2016-03-15 加载Controller，初始化框架

--- src/org/loong4j/framework/HelperLoader.java
+++ src/org/loong4j/framework/HelperLoader.java
@@ -10,13 +10,13 @@ import org.loong4j.framework.util.ClassUtil;
  * Created by loong on 2016/3/15.
  */
 public final class HelperLoader {
-    public static void init(){
-        Class<?>[]classList={ClassHelper.class,
-        BeanHelper.class,
-        IocHelper.class,
-        ControllerHelper.class
+    public static void init() {
+        Class<?>[] classList = {ClassHelper.class,
+                BeanHelper.class,
+                IocHelper.class,
+                ControllerHelper.class
         };
-        for (Class<?> cls:classList){
+        for (Class<?> cls : classList) {
             ClassUtil.loadClass(cls.getName());
         }
     }
